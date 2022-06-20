0f7fa29b358ed807aefd41e84c4adb12e126472c
Ayush <lionelayush446@gmail.com>
2022-06-20 16:06:54 +0530

    adding

---
+++ src/main/java/ayush/springframework/springjokes/SpringJokesApplication.java
@@ -0,0 +1,12 @@
+package ayush.springframework.springjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ayush/springframework/springjokes/SpringJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package ayush.springframework.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
